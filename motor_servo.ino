b202f4f9e8e22306e183fa7128fc82772a2c3f0b
arialfiandi <alfiandiari3@gmail.com>
2024-11-01 21:12:03 +0700

    Add files via upload

---
+++ motor_servo.ino
@@ -0,0 +1,23 @@
+#include <Servo.h>
+
+Servo myServo; // Create a servo object to control a servo motor
+
+int servoPin = 9; // Define the pin to which the servo is connected
+
+void setup() {
+  myServo.attach(servoPin); // Attaches the servo on pin 9 to the servo object
+}
+
+void loop() {
+  // Sweep the servo from 0 to 180 degrees in steps of 1 degree
+  for (int angle = 0; angle <= 180; angle++) {
+    myServo.write(angle); // Set the servo position
+    delay(30); // Wait for servo to reach the position
+  }
+  
+  // Sweep the servo from 180 to 0 degrees in steps of 1 degree
+  for (int angle = 180; angle >= 0; angle--) {
+    myServo.write(angle); // Set the servo position
+    delay(30); // Wait for servo to reach the position
+  }
+}
